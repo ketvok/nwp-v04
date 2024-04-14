4f20e5e1667af838c65b6bbc94ee1337fff8f0a6
Filip Les <ketvok@gmail.com>
2024-04-14 23:30:11 +0200

    Implemented moving functionality.

--- main.cpp
+++ main.cpp
@@ -5,6 +5,16 @@ class ship : public vsite::nwp::window {
 	std::string class_name() override {
 		return "STATIC";
 	}
+	POINT p;
+
+public:
+	void setCoordinates(POINT& pr) {
+		p.x = pr.x;
+		p.y = pr.y;
+	}
+	POINT& getCoordinates() {
+		return p;
+	}
 };
 
 class main_window : public vsite::nwp::window
@@ -15,6 +25,23 @@ class main_window : public vsite::nwp::window
 	bool isMoving = false;
 
 protected:
+	void moveShip(int x, int y) {
+		RECT windowSize{};
+		::GetClientRect(*this, &windowSize);
+
+		if (shipIsCreated) {
+			if (myShip.getCoordinates().x + x >= windowSize.left &&
+				myShip.getCoordinates().x + x + 20 <= windowSize.right &&
+				myShip.getCoordinates().y + y >= windowSize.top &&
+				myShip.getCoordinates().y + y + 20 <= windowSize.bottom) {
+				HWND hw = myShip.operator HWND();
+				::SetWindowPos(hw, 0, myShip.getCoordinates().x + x, myShip.getCoordinates().y + y, 0, 0, SWP_NOSIZE | SWP_NOZORDER);
+				POINT np{ myShip.getCoordinates().x + x, myShip.getCoordinates().y + y };
+				myShip.setCoordinates(np);
+			}
+		}
+	}
+
 	void on_left_button_down(POINT p) override { 
 		// Create ship if it doesn't exist yet.
 		RECT windowSize{};  // Struktura left, right, top, bottom.
@@ -39,6 +66,8 @@ protected:
 				::SetWindowPos(hw, 0, p.x - 10, p.y - 10, 0, 0, SWP_NOSIZE | SWP_NOZORDER);  // Postavi ga na novu poziciju.
 			}
 		}
+		POINT nc{ p.x - 10, p.y - 10 };
+		myShip.setCoordinates(nc);
 	}
 	void on_key_up(int vk) override {
 		if (vk == VK_CONTROL) {
@@ -60,16 +89,16 @@ protected:
 				int moveDistance = ctrlPressed ? 4 : 2;
 				switch (vk) {
 				case VK_LEFT:
-					//moveShip(-moveDistance, 0);
+					moveShip(-moveDistance, 0);
 					break;
 				case VK_RIGHT:
-					//moveShip(moveDistance, 0);
+					moveShip(moveDistance, 0);
 					break;
 				case VK_UP:
-					//moveShip(0, -moveDistance);
+					moveShip(0, -moveDistance);
 					break;
 				case VK_DOWN:
-					//moveShip(0, moveDistance);
+					moveShip(0, moveDistance);
 					break;
 				}
 			}
