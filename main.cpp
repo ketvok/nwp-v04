6bcbd0db68c11ed96318e59efce698426b4ebb18
Filip Les <ketvok@gmail.com>
2024-04-14 14:41:56 +0200

    Prepare class ("STATIC") for a ship. Implement create ship if it doesn't exist yet.

--- main.cpp
+++ main.cpp
@@ -1,12 +1,24 @@
 #include "nwpwin.h"
 
 // TODO: prepare class ("STATIC") for a ship
+class ship : public vsite::nwp::window {
+	std::string class_name() override {
+		return "STATIC";
+	}
+};
 
 class main_window : public vsite::nwp::window
 {
+	ship myShip;
+	static bool shipIsCreated;
+
 protected:
 	void on_left_button_down(POINT p) override { 
 		// TODO: create ship if it doesn't exist yet
+		if (!shipIsCreated) {
+			myShip.create(*this, WS_CHILD | WS_VISIBLE | SS_CENTER | SS_CENTERIMAGE, "x", 0, p.x - 10, p.y - 10, 20, 20);
+		}
+		shipIsCreated = true;
 		// TODO: change current location
 	}
 	void on_key_up(int vk) override {
@@ -21,10 +33,12 @@ protected:
 private:
 };
 
+bool main_window::shipIsCreated = false;
+
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hp, LPSTR cmdLine, int nShow)
 {
 	vsite::nwp::application app;
 	main_window w;
-	w.create(0, WS_OVERLAPPEDWINDOW | WS_VISIBLE, "NWP 4");
+	w.create(0, WS_OVERLAPPEDWINDOW | WS_VISIBLE, "NWP 4", 0, CW_USEDEFAULT, CW_USEDEFAULT, 400, 300);
 	return app.run();
 }
